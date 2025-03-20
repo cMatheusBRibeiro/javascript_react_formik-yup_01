6dc3ece843e0886b79c70459e3c0d597c461a66b
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-03-20 01:03:07 -0300

    Refactor Login component for improved readability and consistency

--- src/paginas/Login/Login.jsx
+++ src/paginas/Login/Login.jsx
@@ -1,38 +1,45 @@
-import styled from "@emotion/styled"
-import { useState } from "react"
-import { Col, Container, Row } from "react-grid-system"
-import { Link as RouterLink } from "react-router-dom"
-import { Botao } from "../../componentes/Botao/Botao"
-import { CampoTexto } from "../../componentes/CampoTexto/CampoTexto"
-import { Card } from "../../componentes/Card/Card"
-import { Link } from "../../componentes/Link/Link"
-import { Tipografia } from "../../componentes/Tipografia/Tipografia"
-import { Logo } from "./Logo"
-import { Form, Formik } from "formik"
+import styled from "@emotion/styled";
+import { useState } from "react";
+import { Col, Container, Row } from "react-grid-system";
+import { Link as RouterLink } from "react-router-dom";
+import { Botao } from "../../componentes/Botao/Botao";
+import { CampoTexto } from "../../componentes/CampoTexto/CampoTexto";
+import { Card } from "../../componentes/Card/Card";
+import { Link } from "../../componentes/Link/Link";
+import { Tipografia } from "../../componentes/Tipografia/Tipografia";
+import { Logo } from "./Logo";
+import { Form, Formik } from "formik";
 
 const FormEstilizado = styled(Form)`
     border-bottom: 1px solid;
-    border-color: ${props => props.theme.cores.primarias.a};
-    padding-bottom: ${props => props.theme.espacamentos.l};
-`
+    border-color: ${(props) => props.theme.cores.primarias.a};
+    padding-bottom: ${(props) => props.theme.espacamentos.l};
+`;
 
 const Login = () => {
-
     const tentarEfetuarLogin = async (evento) => {
         evento.preventDefault();
-        login(email, senha)
-    }
+        login(email, senha);
+    };
 
     return (
-        <Formik initialValues={{ email: '', senha: '' }}>
+        <Formik initialValues={{ email: "", senha: "" }}>
             <Container>
                 <Row justify="center">
-                    <Col xxx={6} xxl={6} xl={6} lg={6} md={8} sm={12} style={{ margin: '80px 0' }}>
-                        <div style={{ textAlign: 'center' }}>
+                    <Col
+                        xxx={6}
+                        xxl={6}
+                        xl={6}
+                        lg={6}
+                        md={8}
+                        sm={12}
+                        style={{ margin: "80px 0" }}
+                    >
+                        <div style={{ textAlign: "center" }}>
                             <Logo />
                         </div>
                         <Card>
-                            <div style={{ textAlign: 'center' }}>
+                            <div style={{ textAlign: "center" }}>
                                 <Tipografia variante="h1" componente="h1">
                                     Efetuar login
                                 </Tipografia>
@@ -40,33 +47,37 @@ const Login = () => {
                             <FormEstilizado onSubmit={tentarEfetuarLogin}>
                                 <CampoTexto
                                     titulo="E-mail"
-                                    name='email'
+                                    name="email"
                                     type="email"
                                 />
                                 <CampoTexto
                                     titulo="Senha"
-                                    name='senha'
+                                    name="senha"
                                     type="password"
                                 />
-                                <div style={{ textAlign: 'right' }}>
+                                <div style={{ textAlign: "right" }}>
                                     <RouterLink to="">
-                                        <Tipografia componente="legenda" variante="legenda">
+                                        <Tipografia
+                                            componente="legenda"
+                                            variante="legenda"
+                                        >
                                             Esqueceu sua senha?
                                         </Tipografia>
                                     </RouterLink>
                                 </div>
-                                <div style={{ textAlign: 'center' }}>
-                                    <Botao>
-                                        Login
-                                    </Botao>
+                                <div style={{ textAlign: "center" }}>
+                                    <Botao>Login</Botao>
                                 </div>
                             </FormEstilizado>
-                            <div style={{ textAlign: 'center' }}>
-                                <Tipografia componente="legenda" variante="legenda">
+                            <div style={{ textAlign: "center" }}>
+                                <Tipografia
+                                    componente="legenda"
+                                    variante="legenda"
+                                >
                                     Ainda não criou sua conta no Freelando?
                                 </Tipografia>
                             </div>
-                            <div style={{ textAlign: 'center' }}>
+                            <div style={{ textAlign: "center" }}>
                                 <Link variante="secundario">
                                     <RouterLink to="/cadastro">
                                         Cadastre-se clicando aqui!
@@ -76,7 +87,9 @@ const Login = () => {
                         </Card>
                     </Col>
                 </Row>
-            </Container></Formik>)
-}
+            </Container>
+        </Formik>
+    );
+};
 
-export default Login
+export default Login;
